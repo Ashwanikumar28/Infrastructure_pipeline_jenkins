b334e6e7566aa7d11986424ec0c559e71391e440
Ashwani <ashwanijha28@gmail.com>
2025-07-22 21:18:58 +0530

    add code to git repo

---
+++ app/variables.tf
@@ -0,0 +1,33 @@
+variable "instance_ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  # default = "ami-09040d770ffe2224f"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  type        = string
+  #default = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+}
+
+
+variable "bucket_name_unique" {
+  description = "unique bucket name"
+  type        = string
+  
+}
+
+variable "bucket_tag_name" {
+  description = "bucket tag name"
+  type        = string
+  
+}
+variable "environment" {
+  description = "environment name"
+  type        = string
+  
+}
