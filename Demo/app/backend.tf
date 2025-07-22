ab352acb4ae76d5021b73d3e25c23136c79727ac
Ashwanikumar28 <infra.2ashwani@gmail.com>
2025-07-22 19:00:55 +0530

    Delete Demo directory

--- Demo/app/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "my-tf-test-bucket-28021992"
-#     key    = "test/terraform.tfstate"
-#     region = "us-east-1"
-#     encrypt        = true                           # encrypt state at rest using SSE
-#     use_lockfile = true        # DynamoDB table for state locking  
-#   }
-# }
--- Demo/modules/ec2/output.tf
+++
@@ -1,7 +0,0 @@
-output "instance_id" {
-  value = aws_instance.web190720.id
-}
-
-output "public_ip" {
-  value = aws_instance.web190720.public_ip
-}
