fc66174d77d32c5ac183d8a1d5e1075150e14f82
Ashwani <ashwanijha28@gmail.com>
2025-07-24 17:14:00 +0530

    formatting 1

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web190720" {
-  ami                         = var.instance_ami
-  instance_type               = var.instance_type
-  vpc_security_group_ids      = [aws_security_group.TF_SG.id]
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.TF_SG.id]
 
   key_name = "eks-game-key"
 
@@ -22,47 +22,47 @@ resource "aws_instance" "web190720" {
 
 
 resource "aws_security_group" "TF_SG" {
-   name        = "security group using Terraform"
-   description = "security group using Terraform"
-   
- 
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
 
-   ingress {
-     description      = "HTTPS"
-     from_port        = 443
-     to_port          = 443
-     protocol         = "tcp"
-     cidr_blocks      = ["0.0.0.0/0"]
-     ipv6_cidr_blocks = ["::/0"]
-   }
 
-   ingress {
-     description      = "HTTP"
-     from_port        = 80
-     to_port          = 80
-     protocol         = "tcp"
-     cidr_blocks      = ["0.0.0.0/0"]
-     ipv6_cidr_blocks = ["::/0"]
-   }
 
-   ingress {
-     description      = "SSH"
-     from_port        = 22
-     to_port          = 22
-     protocol         = "tcp"
-     cidr_blocks      = ["0.0.0.0/0"]
-     ipv6_cidr_blocks = ["::/0"]
-   }
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-   egress {
-     from_port        = 0
-     to_port          = 0
-     protocol         = "-1"
-     cidr_blocks      = ["0.0.0.0/0"]
-     ipv6_cidr_blocks = ["::/0"]
-   }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-   tags = {
-     Name = "TF_SG"
-   }
- }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SG"
+  }
+}
